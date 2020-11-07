f138679c93dab40f9f4ada79e53d73fbfaae1726
Shreesh Kulkarni <sgkul2000@gmail.com>
2020-11-07 11:53:03 +0530

    feat: improved error handling and created a new moule to handle db connection and db checks

--- server.js
+++ server.js
@@ -1,120 +1,115 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
-const dotenv =require('dotenv');
+const express = require('express')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
+const knex = require('knex')
+const morgan = require('morgan')
+
+const db = require('./db')
+
+const app = express()
 
 
-//if (process.env.NODE_ENV !== "production"){
-//  const dotenv = require('dotenv')
-//  dotenv.config();
-//  }
-//console.log(process.env.DB_URI, process.env.DB_USER, process.env.DB_PASS);
-
-const connection = process.env.NODE_ENV === "production"? process.env.DATABASE_URL : {
-  host: '127.0.0.1',
-  user: 'postgres',
-  password: 'databasepost',
-  database:'smart-brain'
-}
-
-
-const db = knex({
-  client: 'pg',
-  connection: connection
-});
-
-const app = express();
-
+app.use(morgan('dev'))
 app.use(cors())
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.get('/', (req, res)=> {
-  res.send(db.users);
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hey👋🏻',
+  })
 })
 
 app.post('/signin', (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
   if (!email || !password) {
-    return res.status(400).json('incorrect form submission');
+    return res.status(400).json('incorrect form submission')
   }
-  db.select('email', 'hash').from('login')
+  console.log(db)
+  db.select('email', 'hash')
+    .from('login')
     .where('email', '=', email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(password, data[0].hash);
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].hash)
       if (isValid) {
-        return db.select('*').from('users')
+        return db
+          .select('*')
+          .from('users')
           .where('email', '=', email)
-          .then(user => {
+          .then((user) => {
             res.json(user[0])
           })
-          .catch(err => res.status(400).json('unable to get user'))
+          .catch((err) => res.status(400).json('unable to get user'))
       } else {
         res.status(400).json('wrong credentials')
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
+    .catch((err) => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
+  const { email, name, password } = req.body
   if (!email || !name || !password) {
-    return res.status(400).json('incorrect form submission');
+    return res.status(400).json('incorrect form submission')
   }
-  const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
+  const hash = bcrypt.hashSync(password)
+  db.transaction((trx) => {
+    trx
+      .insert({
         hash: hash,
-        email: email
+        email: email,
       })
       .into('login')
       .returning('email')
-      .then(loginEmail => {
+      .then((loginEmail) => {
         return trx('users')
           .returning('*')
           .insert({
             email: loginEmail[0],
             name: name,
-            joined: new Date()
+            joined: new Date(),
           })
-          .then(user => {
-            res.json(user[0]);
+          .then((user) => {
+            res.json(user[0])
           })
       })
       .then(trx.commit)
       .catch(trx.rollback)
-    })
-    .catch(err =>{console.log(err)
-      res.status(400).json('unable to register')})
+  }).catch((err) => {
+    console.error(err)
+    res.status(400).json('unable to register')
+  })
 })
 
 app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  db.select('*').from('users').where({id})
-    .then(user => {
+  const { id } = req.params
+  db.select('*')
+    .from('users')
+    .where({ id })
+    .then((user) => {
       if (user.length) {
         res.json(user[0])
       } else {
         res.status(400).json('Not found')
       }
     })
-    .catch(err => res.status(400).json('error getting user'))
+    .catch((err) => res.status(400).json('error getting user'))
 })
 
 app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+  const { id } = req.body
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entries) => {
+      res.json(entries[0])
+    })
+    .catch((err) => res.status(400).json('unable to get entries'))
 })
 
-const port=process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
-app.listen(port , ()=> {
-  console.log(`app is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`app is running on port ${port}`)
 })
